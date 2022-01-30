292f60a5b82f19169ad48f1152fca2ad83262bc1
luying <91367098+lu1hOAO@users.noreply.github.com>
2022-01-30 17:46:02 +0800

    Update and rename 文字框小貓咪.cpp to 文字框小貓咪.c

---
+++ 文字框小貓咪.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+printf("／￣￣￣￣￣￣￣￣￣"\n)
+printf("|　　             "\n)
+printf("＼")
+printf("　￣￣∨￣￣￣￣￣￣"\n)
+printf(" ∧＿∧"\n)
+printf("(　・∀・)"\n)
+printf("(　 つつヾ"\n)
+printf("| ｜ |　我是一隻貓"\n)
+printf("(＿_)＿)"\n)
+  return 0
+  }
